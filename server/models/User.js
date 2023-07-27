f965fc01b1c72a42a70ed8880926c64c675e5d54
Sadaf Al Arabi <itzsdf@gmail.com>
2023-07-27 19:04:59 +0600

    feat(attendance-system): Story-2 Add register and login with authentication system

--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,36 @@
 const {model, Schema} = require('mongoose')
 
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
-    roles: [String],
-    accountStatus: String
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 10
+    },
+    email: {
+        type: String,
+        required: true,
+        validate:{
+            validator: (v) => /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(v),
+            message: (props) => `${props.value} is an invalid email address`
+        },
+    },
+    password: {
+        type: String,
+        minlength: [6, 'Password is too short'],
+        required: true,
+    },
+    roles: {
+        type: [String],
+        required: true,
+        default: ['STUDENT'],
+    },
+    accountStatus: {
+        type: String,
+        enum: ['PENDING', 'ACTIVE', 'REJECTED'],
+        default: 'PENDING',
+        required: true,
+    }
 })
 
 const User = model('User', userSchema)
--- server/server.js
+++ server/server.js
@@ -1,14 +1,71 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const connectDB = require("./db");
+const bcrypt = require("bcryptjs");
+const app = express();
+const User = require("./models/User");
 
-app.get('/', (req, res)=>{
-    const obj = {
-        name: 'ayan',
-        email: 'Shyan@mon.om'
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const obj = {
+    name: "ayan",
+    email: "Shyan@mon.om",
+  };
+  res.json(obj);
+});
+
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
     }
-    res.json(obj)
-})
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    delete user._doc.password
+    return res.status(200).json({ message: "Login Successful", user });
+  } catch (err) {
+    next(err);
+  }
+});
 
-app.listen(4000, ()=>{
-    console.log("Server started on port 4000")
-})
+app.post("/register", async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).json({ message: "Invalid Data" });
+    }
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    user = new User({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    user.password = hash;
+
+    await user.save();
+    res.status(201).json({ message: "User Created Successfully", user });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: "Server error occurred" });
+  console.log(err);
+});
+
+connectDB("mongodb://localhost:27017/attendance-db")
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((e) => console.log(e));
+
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
+});
